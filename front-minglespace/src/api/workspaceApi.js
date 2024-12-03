bce68406dc22984e915c04003a6f0ee6244251c8
codejay2018 <codejay2018@gmail.com>
2024-12-04 03:27:44 +0900

    블랙 리스트 수정 - 포트 설정 원상복구

--- front-minglespace/src/api/workspaceApi.js
+++ front-minglespace/src/api/workspaceApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import Repo from "../auth/Repo";
 
-// export const API_SERVER_HOST = "http://localhost:8080";
-export const API_SERVER_HOST = "http://localhost:8081";
+export const API_SERVER_HOST = "http://localhost:8080";
+// export const API_SERVER_HOST = "http://localhost:8081";
 
 const prefix = `${API_SERVER_HOST}/workspace`;
 
