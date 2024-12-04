22e93810af703850fa62ef398ea41403d4f12fb0
jh0549 <rlawl5544@naver.com>
2024-12-05 04:54:11 +0900

    채팅방 UI  수정

--- front-minglespace/src/chat/components/ChatRoomHeader.js
+++ front-minglespace/src/chat/components/ChatRoomHeader.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { FcExport } from "react-icons/fc";
 
 const ChatRoomHeader = () => {
-  return <div className="chatroom_header">채팅방 헤더</div>;
+  return (
+    <div className="chatroom_header">
+      채팅방 헤더
+      <FcExport />
+    </div>
+  );
 };
 
 export default ChatRoomHeader;
