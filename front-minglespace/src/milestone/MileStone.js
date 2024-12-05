d950141d068a97379fe3a13856b8bbe4916a426d
gksgudgh048733 <gksgudgh048733@gmail.com>
2024-12-05 18:12:06 +0900

    마일스톤 병합
    * 차후 유효성검사 및 UI 수정 필요

--- front-minglespace/src/milestone/MileStone.js
+++ front-minglespace/src/milestone/MileStone.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import MileStoneTest from "./components/MileStoneTest";
+import MileStoneTest from "./components/MileStoneComponent";
 
 const MileStone = () => {
   const [refreshKey, setRefreshKey] = useState(0);
@@ -8,9 +8,7 @@ const MileStone = () => {
     setRefreshKey((prevKey) => prevKey + 1); // 상태 변경으로 강제 재렌더링
   };
 
-  useEffect(() => {
-    console.log(refreshKey);
-  }, [refreshKey]);
+  useEffect(() => {}, [refreshKey]);
   return (
     <div>
       <h1> 마일스톤 페이지</h1>
