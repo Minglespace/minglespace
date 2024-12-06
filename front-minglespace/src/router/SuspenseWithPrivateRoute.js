bcd8f1c679bf225b0c50cea175c1af4ef07e6ea8
codejay2018 <codejay2018@gmail.com>
2024-12-06 12:44:32 +0900

    로그인 유무 체크 하는 페이지 설정 추가

---
+++ front-minglespace/src/router/SuspenseWithPrivateRoute.js
@@ -0,0 +1,16 @@
+import React, { Suspense } from 'react'
+import PrivateRoute from './PrivateRoute';
+
+const Loading = <div>Loading....</div>;
+
+const SuspenseWithPrivateRoute = ({ page: Page }) => {
+  return (
+    <Suspense fallback={Loading}>
+      <PrivateRoute>
+        <Page/>
+      </PrivateRoute>
+    </Suspense>
+  )
+}
+
+export default SuspenseWithPrivateRoute;
