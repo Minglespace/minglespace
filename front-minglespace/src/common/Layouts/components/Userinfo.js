02c138285ed1c901d849a789ead7e30ce07e925f
lsy118 <dldudtn118a@gmail.com>
2024-12-09 11:39:42 +0900

    프론트 약간 추가 수정

--- front-minglespace/src/common/Layouts/components/Userinfo.js
+++ front-minglespace/src/common/Layouts/components/Userinfo.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import ProfileImage from './ProfileImage'
-import Button from './Button'
+import React from "react";
+import ProfileImage from "./ProfileImage";
+import Button from "./Button";
 
-const Userinfo = ({name, role, email, src, title, btnStyle}) => {
-    return (
-        <div className="userInfo_box">
-            <ProfileImage src={src}/>
-            <div className="userInfo_context">
-                <div>
-                    <h2>{name}</h2>
-                    <p>{role}</p>
-                </div>
-                <p>{email}</p>
-            </div>
+const Userinfo = ({ name, role, email, src, title }) => {
+  return (
+    <div className="userInfo_box">
+      <ProfileImage src={src} />
+      <div className="userInfo_context">
+        <div>
+          <h2>{name}</h2>
+          <p>{role}</p>
         </div>
-    )
-}
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Userinfo
+export default Userinfo;
--- front-minglespace/src/myFriends/components/MyFriendsList.js
+++ front-minglespace/src/myFriends/components/MyFriendsList.js
@@ -1,90 +1,92 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import Userinfo from "../../common/Layouts/components/Userinfo";
 import Search from "../../common/Layouts/components/Search";
 import myFriendsApi from "../../api/myFriendsApi";
+import Button from "../../common/Layouts/components/Button";
 
 const userInitData = [
-    {
-        id : 0,
-        email : "",
-        name : "",
-        phone : "",
-        introduction : "",
-        position : "",
-        img : "",
-    },
-]
+  {
+    id: 0,
+    email: "",
+    name: "",
+    phone: "",
+    introduction: "",
+    position: "",
+    img: "",
+  },
+];
 const MyFriendsList = () => {
-    const [user, setUser] = useState([...userInitData]);
-    const [searchKeyword , setSearchKeyword] = useState("");
+  const [user, setUser] = useState([...userInitData]);
+  const [searchKeyword, setSearchKeyword] = useState("");
 
-    const getList = () => {
-        myFriendsApi.getList(searchKeyword).then(
-            (data) =>{
-                setUser(data);
-            }
-        );
+  const getList = () => {
+    myFriendsApi.getList(searchKeyword).then((data) => {
+      setUser(data);
+    });
+  };
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  useEffect(() => {
+    if (isValidKoreanCharacter(searchKeyword) || searchKeyword === "") {
+      const timer = setTimeout(() => {
+        getList(searchKeyword);
+      }, 1000);
+      return () => clearTimeout(timer);
     }
+  }, [searchKeyword]);
 
-    useEffect(()=>{
-        getList();
-    },[])
+  //검색 핸들러
+  const handleSearch = (event) => {
+    setSearchKeyword(event.target.value);
+  };
 
-    useEffect(() => {
-        if(isValidKoreanCharacter(searchKeyword) || searchKeyword ==="") {
-            const timer = setTimeout(() => {
-                getList(searchKeyword);
-            }, 1000);
-            return () => clearTimeout(timer);
-        }
-    }, [searchKeyword]);
+  // 엔터 키 핸들러
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      getList(searchKeyword);
+    }
+  };
 
-    //검색 핸들러
-    const handleSearch = (event) => {
-        setSearchKeyword(event.target.value);
-    };
+  //자음체크
+  const isValidKoreanCharacter = (char) => {
+    const validCharRegex = /^[가-힣a-zA-Z]+$/;
+    return validCharRegex.test(char);
+  };
+  const isCompleteKoreanString = (str) => {
+    for (let char of str) {
+      if (!isValidKoreanCharacter(char)) return false;
+    }
+    return true;
+  };
 
-    // 엔터 키 핸들러
-    const handleKeyDown = (event) =>{
-       if (event.key === 'Enter') {
-           event.preventDefault();
-              getList(searchKeyword);
-          }
-     }
-
-    //자음체크
-    const isValidKoreanCharacter = (char) => {
-        const validCharRegex = /^[가-힣a-zA-Z]+$/;
-     return validCharRegex.test(char);
-    };
-    const isCompleteKoreanString = (str) => {
-        for (let char of str){
-         if (!isValidKoreanCharacter(char))
-            return false;
-        }
-        return true;
-    };
-
-    return (
-        <div className="section_container myFriends_container_item">
-            <h1 className="section_container_title">My Friends</h1>
-            <Search placeholder={"이름을 검색하세요"}
-                    onSearch={handleSearch}
-                    onKeyDown={handleKeyDown}/>
-            <div className="myFriends_userInfo_container">
-            {
-                user.map((userInfo) => (
-                    <Userinfo
-                        key={userInfo.id}
-                        name={userInfo.name}
-                        role={userInfo.position}
-                        email={userInfo.email}
-                        src={userInfo.img}/>
-                ))
-            }
-            </div>
-        </div>
-    );
+  return (
+    <div className="section_container myFriends_container_item">
+      <h1 className="section_container_title">My Friends</h1>
+      <Search
+        placeholder={"이름을 검색하세요"}
+        onSearch={handleSearch}
+        onKeyDown={handleKeyDown}
+      />
+      <div className="myFriends_userInfo_container">
+        {user.map((userInfo) => (
+          <div className="myFriends_userInfo_deleteButton">
+            <Userinfo
+              key={userInfo.id}
+              name={userInfo.name}
+              role={userInfo.position}
+              email={userInfo.email}
+              src={userInfo.img}
+            />
+            <Button btnStyle="exit_button" title="친구삭제" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyFriendsList;
