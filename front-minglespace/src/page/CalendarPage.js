99dc8500190122346be03d21bf6b4468f3cc2451
lsy118 <dldudtn118a@gmail.com>
2024-12-17 10:23:58 +0900

    캘린더 공통 커밋

---
+++ front-minglespace/src/page/CalendarPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BasicLayout from "../common/Layouts/BasicLayout";
+import Calendar from "../calendar/Calendar";
+
+const CalendarPage = () => {
+  return (
+    <BasicLayout>
+      <Calendar />
+    </BasicLayout>
+  );
+};
+
+export default CalendarPage;
