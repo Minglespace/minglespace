903e88b546f81677f7c2e9eaa02e5162360b9c35
lsy118 <dldudtn118a@gmail.com>
2024-12-23 14:48:19 +0900

    초대 경로수정

--- front-minglespace/src/router/SuspenseWithPrivateRoute.js
+++ front-minglespace/src/router/SuspenseWithPrivateRoute.js
@@ -1,16 +1,25 @@
-import React, { Suspense } from 'react'
-import PrivateRoute from './PrivateRoute';
+import React, { Suspense, useEffect } from "react";
+import PrivateRoute from "./PrivateRoute";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Loading = <div>Loading....</div>;
 
 const SuspenseWithPrivateRoute = ({ page: Page }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const currentPath = location.pathname;
+    navigate(currentPath, { state: { from: currentPath } });
+  }, []);
+
   return (
     <Suspense fallback={Loading}>
       <PrivateRoute>
-        <Page/>
+        <Page />
       </PrivateRoute>
     </Suspense>
-  )
-}
+  );
+};
 
 export default SuspenseWithPrivateRoute;
