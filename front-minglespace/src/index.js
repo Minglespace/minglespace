18dd94abf19c9753b192b0c05277bf0257965db5
lsy118 <dldudtn118a@gmail.com>
2024-12-30 10:55:41 +0900

    error처리

--- front-minglespace/src/index.js
+++ front-minglespace/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./asset/scss/style.scss";
-import ErrorBoundary from "./common/Exception/ErrorBoundary";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
